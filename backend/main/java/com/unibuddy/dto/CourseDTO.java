fa345ab9d4ba282648a505e930d96166e266e2ab
mponiente <michaelwrites.biz@gmail.com>
2025-10-23 22:11:30 -0700

    setup the structure - no code

---
+++ backend/main/java/com/unibuddy/dto/CourseDTO.java
@@ -0,0 +1,5 @@
+package backend.main.java.com.unibuddy.dto;
+
+public class CourseDTO {
+    
+}
---
+++ backend/main/java/com/unibuddy/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package backend.main.java.com.unibuddy.exception;
+
+public class NotFoundException {
+    
+}
