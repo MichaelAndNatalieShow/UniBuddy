1af700e804929765ca33b3e1fb2990a24f849a3e
mponiente <michaelwrites.biz@gmail.com>
2025-10-24 20:23:33 -0700

    course class

--- backend/main/java/com/unibuddy/models/Course.java
+++ backend/main/java/com/unibuddy/models/Course.java
@@ -1,12 +1,24 @@
 package backend.main.java.com.unibuddy.models;
 
-// import jakarta.persistence.*;
-// import lombok.*;
+ import jakarta.persistence.*;
+ import lombok.*;
 
-// @Entity
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
+ @Entity
+ @Data
+ @NoArgsConstructor
+ @AllArgsConstructor
 public class Course {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long courseId;
+    private String courseDepartment;
+    private String courseTitle;
+    private int courseCredits;
+    private boolean offeredFall;
+    private boolean offeredSpring;
+    private boolean offeredSummer;
+    private String calgetcArea;
+    private String breadthCategory;
 }
