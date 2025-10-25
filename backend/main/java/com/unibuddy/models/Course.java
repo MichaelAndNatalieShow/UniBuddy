1a56f8abe9a6df6a661b3ae654cdd931d8b2af4f
mponiente <michaelwrites.biz@gmail.com>
2025-10-24 19:42:01 -0700

    idk

--- backend/main/java/com/unibuddy/models/Course.java
+++ backend/main/java/com/unibuddy/models/Course.java
@@ -1,5 +1,12 @@
 package backend.main.java.com.unibuddy.models;
 
+// import jakarta.persistence.*;
+// import lombok.*;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
 public class Course {
     
 }
