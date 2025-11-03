c7035c81159e6e106fd9cdb18544a1914e2c5135
mponiente <michaelwrites.biz@gmail.com>
2025-11-03 00:21:51 -0800

    fixed deployment issues overall

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,12 @@ SECRET_KEY = 'django-insecure-srzlq!phsern^#)j5rbbr-4ex#f00^6v08jvz86jh6gykla&*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+# backend/settings.py
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "unibuddy.up.railway.app",
+]
 
 
 # Application definition
